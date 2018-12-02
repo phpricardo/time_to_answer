cc8754eeec158fa83c603bccfd5f3a0609788ca3
Ricardo Lima <phpricardo@gmail.com>
2018-12-02 17:31:54 -0200

    First commit of application

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+
+  devise_for :admins
+  devise_for :users
+
+  root 'site/welcome#index'
+end
