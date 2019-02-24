581085d78ecaa346fc167c4b135d030700e56fad
Ricardo Lima <phpricardo@gmail.com>
2019-02-24 14:24:30 -0300

    crud das questoes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     get 'welcome/index' # Dashboard
     resources :admins # Administradores
     resources :subjects # Assuntos e Áreas
+    resources :questions # Perguntas
   end
   
   devise_for :admins
