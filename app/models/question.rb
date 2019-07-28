f9b57273acd6e3f0872ff342d1dda355b18233ca
Ricardo Lima <phpricardo@gmail.com>
2019-07-28 18:51:52 -0300

    apply class method in models

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,16 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
+  # pagination with kaminari
   paginates_per 5
+
+  def self.search(term, page)
+    Question.includes(:answers)
+            .where("lower(description) LIKE ?", "%#{term.downcase}%")
+            .page(page)
+  end
+
+  def self.last_questions(page)
+    Question.includes(:answers).order('created_at desc').page(params[:page])
+  end
 end
