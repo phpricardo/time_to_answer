ea18e4462d00e14852ff70e4d01fb346744ea19a
Ricardo Lima <phpricardo@gmail.com>
2019-07-28 16:48:35 -0300

    add search bar and more layout modifications

--- app/controllers/site/welcome_controller.rb
+++ app/controllers/site/welcome_controller.rb
@@ -1,4 +1,5 @@
-class Site::WelcomeController < ApplicationController
+class Site::WelcomeController < SiteController
   def index
+    @questions = Question.includes(:answers).page(params[:page])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
+    get 'search', to: 'search#questions'
   end
   namespace :users_backoffice do
     get 'welcome/index'
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
   devise_for :users
  
   get 'inicio', to: 'site/welcome#index'
+  get 'backoffice', to: 'admins_backoffice/welcome#index'
 
   root to: 'site/welcome#index'
 end
