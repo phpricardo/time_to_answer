51a682064f56d0b2a55f3e52c73a57d5c6f67351
Ricardo Lima <ricardo.lima@easypallet.com.br>
2019-10-24 18:50:37 -0300

    add first and last name in devise, update date and using validations rails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Validations
+  validates :first_name, presence: true, length: { minimum: 3 , on: :update }
+
+  # virtual attributes
+  def full_name
+    #"#{self.first_name} #{self.last_name}"
+    [self.first_name, self.last_name].join(' ')
+  end
 end
